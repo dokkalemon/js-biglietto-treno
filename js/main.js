b1f21b45971a67d43730067f390ac6e44122f4a0
dokkalemon <dottori.roberto@gmail..com>
2021-10-21 15:46:00 +0200

    add underage discount rules

--- js/main.js
+++ js/main.js
@@ -10,11 +10,15 @@ console.log('Età Viaggiatore', age);
 console.log(typeof age);
 
 //prezzo biglietto
-let price = distance * 0.21;
-console.log(price);
-console.log(typeof price);
+let normalPrice = distance * 0.21;
+console.log('Prezzo Intero',normalPrice);
+console.log(typeof normalPrice);
 
 //sconto 20% se minorenne
+const underageDiscount = normalPrice * 20 / 100;
+console.log('Sconto Minorenni', underageDiscount);
+
+
 //sconto 40% se over 65
 // calcolo prezzo finale
 //output
