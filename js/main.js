4327c9b2b0690c95493cfddbdd042359e78bce97
dokkalemon <dottori.roberto@gmail..com>
2021-10-21 16:15:28 +0200

    end

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const distance = parseInt( prompt('Quanti chilometri devi percorrere?') );
 console.log('Distanza da percorrere', distance);
 console.log(typeof distance);
 
-
 //chidere età del passeggero
 const age = parseInt( prompt('Quale è la tua età?'));
 console.log('Età Viaggiatore', age);
@@ -11,14 +10,34 @@ console.log(typeof age);
 
 //prezzo biglietto
 let normalPrice = distance * 0.21;
+let normalPriceRounded = normalPrice.toFixed(2);
 console.log('Prezzo Intero',normalPrice);
 console.log(typeof normalPrice);
 
 //sconto 20% se minorenne
-const underageDiscount = normalPrice * 20 / 100;
+const underageDiscount = normalPrice * 0.2;
 console.log('Sconto Minorenni', underageDiscount);
 
-
 //sconto 40% se over 65
-// calcolo prezzo finale
+const adultDiscount = normalPrice * 0.4;
+console.log('Sconto Over 65', adultDiscount);
+
+/* calcolo prezzo finale */
+//PREZZO MINORENNI
+const underagePrice = normalPrice - underageDiscount;
+const underagePriceRounded = underagePrice.toFixed(2);
+console.log('Prezzo per minorenni', underagePrice);
+
+//PREZZO OVER 65
+const adultPrice = normalPrice - adultDiscount;
+let adultPriceRounded = adultPrice.toFixed(2);
+console.log('Prezzo per over 65', adultPrice);
+
 //output
+if (age < 18) {
+    document.getElementById('result').innerHTML = `Il prezzo del biglietto è ${underagePriceRounded}€`;
+} else if (age > 65) {
+    document.getElementById('result').innerHTML = 'Il prezzo del biglietto è ' + adultPriceRounded + '€';
+} else {
+    document.getElementById('result').innerHTML = `Il prezzo del biglietto è ${normalPriceRounded}€`
+}
