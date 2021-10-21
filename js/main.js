018b1d2bb73b0f33d85439df3711c3144dd8054c
dokkalemon <dottori.roberto@gmail..com>
2021-10-21 15:31:03 +0200

    add age prompt

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ console.log(typeof distance)
 
 
 //chidere età del passeggero
+const age = parseInt( prompt('Quale è la tua età?'))
+console.log('Età Viaggiatore', age)
+console.log(typeof age)
+
 //prezzo biglietto
 //sconto 20% se minorenne
 //sconto 40% se over 65
