40f6038012d9565016b2c90adbc755810eb4a70c
dokkalemon <dottori.roberto@gmail..com>
2021-10-21 15:32:32 +0200

    fix js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //chiedere numero di chilometri
-const distance = parseInt( prompt('Quanti chilometri devi percorrere?') )
-console.log('Distanza da percorrere', distance)
-console.log(typeof distance)
+const distance = parseInt( prompt('Quanti chilometri devi percorrere?') );
+console.log('Distanza da percorrere', distance);
+console.log(typeof distance);
 
 
 //chidere età del passeggero
-const age = parseInt( prompt('Quale è la tua età?'))
-console.log('Età Viaggiatore', age)
-console.log(typeof age)
+const age = parseInt( prompt('Quale è la tua età?'));
+console.log('Età Viaggiatore', age);
+console.log(typeof age);
 
 //prezzo biglietto
 //sconto 20% se minorenne
